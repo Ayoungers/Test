abbf88a255a1c016d29d4cdfb189b633e7105d50
Glagha <combustable@gmail.com>
2018-10-16 22:33:11 -0500

    Initial commit of assets Added basic fist movement and collision detection

---
+++ Assets/Scripts/FistController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FistController : MonoBehaviour {
+
+    private Vector3 mousePosition;
+    private Vector2 mouseVector;
+
+    private GameObject hitObject;
+    private Rigidbody2D hitRb;
+    public float moveSpeed = 0.1f;
+    public float maxSpeed = 1f;
+
+	//// Use this for initialization
+	//void Start () {
+		
+	//}
+	
+	// Update is called once per frame
+	void Update () {
+        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        gameObject.GetComponent<Rigidbody2D>().velocity = (mousePosition - transform.position).normalized * moveSpeed;
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        mouseVector = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+
+        hitObject = collision.gameObject;
+        hitRb = gameObject.GetComponent<Rigidbody2D>();
+        hitRb.AddForce(mouseVector);
+    }
+}
